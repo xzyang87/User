2598c2677f3abea667dbd385ca212f95e75efd26
Xiaozhu Yang <xzyang@thoughtworks.com>
2020-06-15 11:11:35 +0800

    created the test file for users

---
+++ demo/src/test/java/com/xzyangjnzheng/demo/UsersControllerTest.java
@@ -0,0 +1,27 @@
+package com.xzyangjnzheng.demo;
+
+import static org.hamcrest.Matchers.equalTo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UsersControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getUsers() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("123")));
+    }
+}
