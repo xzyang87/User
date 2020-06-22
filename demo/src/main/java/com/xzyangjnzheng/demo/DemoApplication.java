41a7a102a8acdb1ee7d77963cbb25cab4643e6cf
Xiaozhu Yang <xzyang@thoughtworks.com>
2020-06-22 21:10:37 +0800

    Revert "introduced Eureka server dependency, still some minor issue"
    This reverts commit ddee14b66e991147b25b39ae4dc8acc7dd52843d.

--- demo/src/main/java/com/xzyangjnzheng/demo/DemoApplication.java
+++ demo/src/main/java/com/xzyangjnzheng/demo/DemoApplication.java
@@ -3,19 +3,13 @@ package com.xzyangjnzheng.demo;
 import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 
 @SpringBootApplication
 @Generated
-@EnableEurekaServer
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(DemoApplication.class).web(true).run(args);
-//		SpringApplication.run(DemoApplication.class, args
-		);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
